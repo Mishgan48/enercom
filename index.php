0409d56432e35c03676107cf766844bb7dcf7d0e
Mike <mike333schum@gmail.com>
2019-04-24 12:04:01 +0300

    Заглушка готова

--- index.php
+++ index.php
@@ -2,14 +2,15 @@
 <html lang="ru" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Enercom Robots</title>
+    <title>Enercom Robotics</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="keywords" content="">
-    <meta name="description" content="">
+    <meta name="keywords" content="робот, интеграция, производство, автоматизация, липецк">
+    <meta name="description" content="Центр разработки и интеграции роботизированных решений">
 
-    <link rel="stylesheet" type="text/css" href="fonts/Geometria/Geometria.css" />
-    <link rel="stylesheet" type="text/css" href="fonts/eurostile-extended/eurostile-extended.css" />
-    <link rel="stylesheet" type="text/css" href="style/style.css" />
+    <link rel="icon" type="image/png" href="img/favicon.png">
+    <link rel="stylesheet" href="fonts/Geometria/Geometria.css" />
+    <link rel="stylesheet" href="fonts/eurostile-extended/eurostile-extended.css" />
+    <link rel="stylesheet" href="style/style.css" />
   </head>
 
   <body>
@@ -22,7 +23,7 @@
         <div class="top__contact">+7 (4742) 48-00-18<br>office@enercom-robotics.com</div>
       </div>
 
-      <h1>ЦЕНТР РАЗРАБОТКИ И ИНТЕРГРАЦИИ<br><span class="bold">РОБОТИЗИРОВАННЫХ ТЕХНИЧЕСКИХ РЕШЕНИЙ</span></h1>
+      <h1>ЦЕНТР РАЗРАБОТКИ И ИНТЕГРАЦИИ<br><span class="bold">РОБОТИЗИРОВАННЫХ РЕШЕНИЙ</span></h1>
 
       <form class="top__form" action="">
         <p class="top__info">Сайт находится в разработке.<br>Будем рады ответить на все вопросы:</p>
